54e8e19e2d97d098cb64b50c29c02db7ff5f777d
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-06 20:40:00 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,40 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    sql = "PRAGMA table_info(#{table_name})"
+    test = DB[:conn].execute(sql)
+    column_names = []
+    test.each do |column|
+      column_names << column["name"]
+    end
+    column_names.compact
+
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col|
+      values << "'#{send(col)}'" unless send(col).nil?
+    end
+    binding.pry
+    values.join
+  end
+
+
 end
